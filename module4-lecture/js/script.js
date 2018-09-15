b02b0cc7b57ab2f4d23c6a94c01f6d8f98d911d7
Akshay Kalola <akshay.kalola28@gmail.com>
2018-09-15 22:38:40 +0530

    Module 5 Solution Starter

--- module4-lecture/js/script.js
+++ module4-lecture/js/script.js
@@ -1,9 +1,11 @@
-var a = {
-	x: 10
-}
-var b = a;
-console.log("a: " + a.x);
-console.log("b: " + b.x);
-b.x  = 20;
-console.log("a: " + a.x);
-console.log("b: " + b.x);
+var arr = new Array();
+arr[0]=123;
+arr[1]="akshay";
+arr[3]="akshay";
+arr[2]=123;
+
+console.log(arr.length);
+
+arr.concat(123);
+console.log(arr.length);
+console.log(arr.toString());
---
+++ module5-lecture/js/script.js
@@ -0,0 +1,10 @@
+// Collapse Button JQuery
+$(function () {
+	
+	$("#navbarToggle").blur(function (event) {
+		var screenWidth = window.innerWidth;
+		if (screenWidth < 768) {
+			$("#collapsable-nav").collapse('hide');
+		}
+	});
+})
